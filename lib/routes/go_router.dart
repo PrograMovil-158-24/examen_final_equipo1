7b71570619a79237ff0d300eb427ba732f362403
Cristian Henriquez <c.henriquez_94@hotmail.com>
2024-03-22 21:26:19 -0600

    misc
    removed unused file; added go router and vehicle list screen

---
+++ lib/routes/go_router.dart
@@ -0,0 +1,13 @@
+import 'package:examen_final_equipo1/screens/vehicle_list.dart';
+import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
+
+final GoRouter router = GoRouter(
+  routes: <RouteBase>[
+    GoRoute(
+        path: '/',
+        builder: (BuildContext context, GoRouterState state) {
+          return const VehicleList();
+        }),
+  ],
+);
--- lib/screens/vehicle_list.dart
+++ lib/screens/vehicle_list.dart
@@ -1,1 +1,18 @@
 import 'package:flutter/material.dart';
+
+class VehicleList extends StatelessWidget {
+  const VehicleList({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: const Text('Vehicle List'),
+        ),
+        body: const Column(
+          children: [Text('Here is a vehicle list')],
+        ));
+  }
+}
