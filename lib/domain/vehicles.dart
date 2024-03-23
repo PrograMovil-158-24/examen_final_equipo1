babdaf7ecd1ab57b1373c1c63cf31e6a88c27623
Cristian Henriquez <c.henriquez_94@hotmail.com>
2024-03-22 20:43:42 -0600

    Create vehicles.dart

---
+++ lib/domain/vehicles.dart
@@ -0,0 +1,15 @@
+class Vehicle {
+  String brand;
+  String model;
+  int year;
+  double price;
+  String photoUrl;
+
+  Vehicle({
+    required this.brand,
+    required this.model,
+    required this.year,
+    required this.price,
+    required this.photoUrl,
+  });
+}
